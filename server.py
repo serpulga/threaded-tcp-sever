691328d89f3c300bea1ea52bafcafb74f4a30336
Sergio <serpulga@gmail.com>
2013-02-22 16:55:07 -0500

    Not using exit as function name.

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ from PySide.QtCore import QCoreApplication
 
 from threadedserver import Server
 
-def exit(signal, frame):
+def exit_handler(signal, frame):
     print 'Exiting!'
     sys.exit(0)
 
@@ -15,7 +15,7 @@ def main():
     app = QCoreApplication(sys.argv)
     server = Server()
     
-    signal.signal(signal.SIGINT, exit)
+    signal.signal(signal.SIGINT, exit_handler)
     return app.exec_()
     
 if __name__ == '__main__':
